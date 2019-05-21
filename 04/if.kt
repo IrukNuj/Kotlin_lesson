4bfd6addb8c0306a7e719523b40937fb8b8aa84b
Admin <b1716568@planet.kanazawa-it.ac.jp>
2019-05-22 00:40:34 +0900

    add: 04 if分について書きました

---
+++ 04/if.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    val one: Int = 1
+    val two: Int = 2
+    val three: Int = 3
+    // それぞれの変数が奇数か偶数かを判定したい...！
+
+    // 条件を満たしていないからprintlnは実行されない
+    if (one % 2 == 0) {
+        println("oneは偶数！")
+    }
+
+    // 条件を満たさないときとしてelseをかける
+    if (two % 2 == 0) {
+        println("twoは偶数！")
+    } else {
+        println("twoは奇数！")
+    }
+
+    //elseに対しても条件指定はできるよ
+    if (three % 2 == 0) {
+        println("threeは偶数！")
+    } else if(three % 2 != 0) {
+        println("threeは奇数！")
+    }
+}
